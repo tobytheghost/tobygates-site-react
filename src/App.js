9c05aac5de10178a91e2c91eb6823f3080baa031
tobytheghost <tobytheghost@hotmail.com>
2021-01-22 22:02:33 +0000

    Add lazy load components

--- src/App.js
+++ src/App.js
@@ -1,27 +1,42 @@
-import React from "react";
+import React, { Suspense, lazy } from "react";
 import {
   Header,
-  Footer,
-  About,
-  //MyWork,
-  Contact,
-  Technologies,
+  // Footer,
+  // About,
+  // //MyWork,
+  // Contact,
+  // Technologies,
 } from "./components";
 
 //Styles
 import "./App.scss";
+
+const Footer = lazy(() => import("./components/Footer/Footer"));
+const About = lazy(() => import("./components/About/About"));
+const Contact = lazy(() => import("./components/Contact/Contact"));
+const Technologies = lazy(() =>
+  import("./components/Technologies/Technologies")
+);
 
 function App() {
   return (
     <div className="App">
       <Header />
       <main className="main">
-        <About />
-        <Technologies />
+        <Suspense fallback={<div>Loading ...</div>}>
+          <About />
+        </Suspense>
+        <Suspense fallback={<div>Loading ...</div>}>
+          <Technologies />
+        </Suspense>
         {/* <MyWork /> */}
-        <Contact />
+        <Suspense fallback={<div>Loading ...</div>}>
+          <Contact />
+        </Suspense>
       </main>
-      <Footer />
+      <Suspense fallback={<div>Loading ...</div>}>
+        <Footer />
+      </Suspense>
     </div>
   );
 }
